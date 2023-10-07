08c9e9d3e46ed0acb7f351430a7b64232a3572d6
Burak Akgün <burak@mbakgun.com>
2023-10-07 20:55:45 +0200

    feature/image-loader-improvements (#107)
    * Update ImageLoader loading

--- shared/src/commonMain/kotlin/util/ImageLoader.kt
+++ shared/src/commonMain/kotlin/util/ImageLoader.kt
@@ -1,7 +1,5 @@
 package util
 
-import androidx.compose.runtime.Composable
 import com.seiko.imageloader.ImageLoader
 
-@Composable
 expect fun generateImageLoader(): ImageLoader
--- shared/src/iosMain/kotlin/util/ImageLoader.kt
+++ shared/src/iosMain/kotlin/util/ImageLoader.kt
@@ -1,25 +1,36 @@
 package util
 
-import androidx.compose.runtime.Composable
 import com.seiko.imageloader.ImageLoader
-import com.seiko.imageloader.cache.memory.maxSizePercent
-import com.seiko.imageloader.component.ComponentRegistryBuilder
 import com.seiko.imageloader.component.setupDefaultComponents
-import okio.FileSystem
+import com.seiko.imageloader.defaultImageResultMemoryCache
+import okio.Path.Companion.toPath
+import platform.Foundation.NSCachesDirectory
+import platform.Foundation.NSSearchPathForDirectoriesInDomains
+import platform.Foundation.NSUserDomainMask
 
-@Composable
-actual fun generateImageLoader(): ImageLoader =
-    ImageLoader {
-        components(ComponentRegistryBuilder::setupDefaultComponents)
+actual fun generateImageLoader(): ImageLoader {
+    return ImageLoader {
+        components {
+            setupDefaultComponents()
+        }
         interceptor {
-
+            // cache 100 success image result, without bitmap
+            defaultImageResultMemoryCache()
             memoryCacheConfig {
-                maxSizePercent(0.55)
+                maxSizeBytes(32 * 1024 * 1024) // 32MB
             }
-
             diskCacheConfig {
-                directory(FileSystem.SYSTEM_TEMPORARY_DIRECTORY)
-                maxSizeBytes(1024 * 1024 * 100)
+                directory(getCacheDir().toPath().resolve("image_cache"))
+                maxSizeBytes(512L * 1024 * 1024) // 512MB
             }
         }
     }
+}
+
+private fun getCacheDir(): String {
+    return NSSearchPathForDirectoriesInDomains(
+        NSCachesDirectory,
+        NSUserDomainMask,
+        true,
+    ).first() as String
+}
