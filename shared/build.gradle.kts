c1b88ace9d7dc6a2cb92b88da9f60f9ce007478e
Mehmet Burak Akgün <burak.akgun@trendyol.com>
2024-05-22 19:46:45 +0200

    Fix failed unit tests

--- shared/build.gradle.kts
+++ shared/build.gradle.kts
@@ -90,6 +90,7 @@ kotlin {
         }
 
         commonTest.dependencies {
+            implementation(kotlin("test"))
             implementation(libs.kotlinxCoroutinesTest)
             implementation(libs.koinTest)
         }
