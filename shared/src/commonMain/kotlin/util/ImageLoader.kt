7943e76d970680c75951bf87c484f2a7e3f23bdf
Mehmet Burak Akgün <burak.akgun@trendyol.com>
2024-05-23 19:52:58 +0200

    Remove app context from android variants and remove disk cache (web/wasm)

--- shared/src/commonMain/kotlin/util/ImageLoader.kt
+++ shared/src/commonMain/kotlin/util/ImageLoader.kt
@@ -2,19 +2,9 @@ package util
 
 import coil3.ImageLoader
 import coil3.PlatformContext
-import coil3.disk.DiskCache
 import coil3.request.CachePolicy
 import coil3.util.DebugLogger
-import okio.FileSystem
 
 fun getAsyncImageLoader(context: PlatformContext) =
     ImageLoader.Builder(context).diskCachePolicy(CachePolicy.ENABLED)
-        .networkCachePolicy(CachePolicy.ENABLED).diskCache {
-        newDiskCache()
-    }.logger(DebugLogger()).build()
-
-fun newDiskCache(): DiskCache {
-    return DiskCache.Builder().directory(FileSystem.SYSTEM_TEMPORARY_DIRECTORY / "image_cache")
-        .maxSizeBytes(512L * 1024 * 1024)
-        .build()
-}
+        .networkCachePolicy(CachePolicy.ENABLED).logger(DebugLogger()).build()
