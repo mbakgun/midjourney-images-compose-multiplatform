e8c64af4f09d00325b1256d72cb8fcd77732537c
Mehmet Burak Akgün <burak.akgun@trendyol.com>
2024-05-24 19:21:40 +0200

    Impl responsive design

--- shared/src/iosMain/kotlin/util/ImageProvider.kt
+++ shared/src/iosMain/kotlin/util/ImageProvider.kt
@@ -1,8 +1,9 @@
 package util
 
+import androidx.compose.ui.unit.Dp
 import androidx.compose.ui.unit.dp
 
-internal actual fun getImageProvider(): ImageProvider =
+internal actual fun getImageProvider(width: Dp): ImageProvider =
     ImageProvider(
         columnCount = 2,
         itemHeight = 180.dp
--- shared/src/wasmJsMain/kotlin/util/ImageProvider.js.kt
+++ shared/src/wasmJsMain/kotlin/util/ImageProvider.js.kt
@@ -1,9 +1,20 @@
 package util
 
+import androidx.compose.ui.unit.Dp
 import androidx.compose.ui.unit.dp
 
-internal actual fun getImageProvider(): ImageProvider =
+internal actual fun getImageProvider(width: Dp): ImageProvider =
     ImageProvider(
-        columnCount = 4,
+        columnCount = fromWidth(width),
         itemHeight = 400.dp
     )
+
+fun fromWidth(width: Dp): Int {
+    return if (width < 600.dp) {
+        2
+    } else if (width < 1200.dp) {
+        3
+    } else {
+        4
+    }
+}
