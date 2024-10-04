b6169e909ae943722ceb0b9832c230db7c0e98df
Mehmet Burak Akgün <burak.akgun@trendyol.com>
2024-10-04 07:06:26 +0200

    Update image height for web

--- shared/src/jsMain/kotlin/util/ImageProvider.js.kt
+++ shared/src/jsMain/kotlin/util/ImageProvider.js.kt
@@ -3,11 +3,20 @@ package util
 import androidx.compose.ui.unit.Dp
 import androidx.compose.ui.unit.dp
 
-internal actual fun getImageProvider(width: Dp): ImageProvider =
-    ImageProvider(
-        columnCount = fromWidth(width),
-        itemHeight = 400.dp
+internal actual fun getImageProvider(width: Dp): ImageProvider {
+    val columnCount = fromWidth(width)
+    return ImageProvider(
+        columnCount = columnCount,
+        itemHeight = getItemHeightByColumn(columnCount)
     )
+}
+
+private fun getItemHeightByColumn(columnCount: Int) = when (columnCount) {
+    2 -> 180.dp
+    3 -> 300.dp
+    4 -> 400.dp
+    else -> throw IllegalArgumentException("Unsupported column count: $columnCount")
+}
 
 fun fromWidth(width: Dp): Int {
     return if (width < 1080.dp) {
